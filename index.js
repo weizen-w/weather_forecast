ece18094fafc37ef077ad77538c401eb9fa64d76
Wladimir Weizen <weizen.w@gmx.de>
2023-07-22 21:08:40 +0200

    style: add code style and rename files

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
-const divElement = document.getElementById("div");
+const divElement = document.getElementById("div-weather-IP");
+// paths to icons
+const iconPathBackgroundDay = "./image/bg-day.jpg";
+const iconPathBackgroundNight = "./image/bg-night.jpg";
+const iconTemperatur = "./image/icon-temp.png";
+const iconWind = "./image/icon-wind.png";
+const iconArrow = "./image/icon-arrow.png";
+  // icons to value weather code
+const iconPathClearSky = "./image/codeWeather/clearSky.png";
+const iconPathMainly = "./image/codeWeather/mainly.png";
+const iconPathFog = "./image/codeWeather/fog.png";
+const iconPathDrizzle = "./image/codeWeather/drizzle.png";
+const iconPathFreezingDrizzle = "./image/codeWeather/freezingDrizzle.png";
+const iconPathRain = "./image/codeWeather/rain.png";
+const iconPathFreezingRain = "./image/codeWeather/freezingRain.png";
+const iconPathSnow = "./image/codeWeather/snow.png";
+const iconPathRainShowers = "./image/codeWeather/rainShowers.png";
+const iconPathSnowHeavy = "./image/codeWeather/snowHeavy.png";
+const iconPathThunderstorm = "./image/codeWeather/thunderstorm.png";
+const iconPathThunderstormHail = "./image/codeWeather/thunderstormHail.png";
 
-function getBGImageFromTime(time) {
-  const hour = time.substring(11, 13);
-  if (hour >= 8 && hour < 20) return "./image/bg-day.jpg";
-  if (hour >= 20 || hour < 8) {
-    divElement.style.color = "white";
-    return "./image/bg-night.jpg";
-  }
+getIPandWeather();
+
+/**
+ * 
+ */
+async function getIPandWeather() {
+  const resIP = await fetch("https://get.geojs.io/v1/ip/geo.json");
+  const objectIP = await resIP.json();
+  const { longitude, latitude } = objectIP;
+
+  const resWeather = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`);
+  const objectWeather = await resWeather.json();
+
+  addHTMLelements(objectIP, objectWeather);
 }
 
+/**
+ * 
+ * @param {*} time 
+ * @returns 
+ */
+function getBGImageFromTime(time) {
+  const hour = time.substring(11, 13);
+  if (hour >= 8 && hour < 20) return iconPathBackgroundDay;
+  if (hour >= 20 || hour < 8) return iconPathBackgroundNight;
+}
+
+/**
+ * 
+ * @param {*} winddirection 
+ * @returns 
+ */
 function getImageWindDirection(winddirection) {
   if (winddirection < 22.5 || winddirection > 337.5) return "North";
   if (winddirection < 67.5 && winddirection > 22.5) return "North-east";
@@ -20,32 +62,114 @@ function getImageWindDirection(winddirection) {
   if (winddirection < 337.5 && winddirection > 292.5) return "North-west";
 }
 
+/**
+ * 
+ * @param {*} code 
+ * @returns 
+ */
+function getWeatherCodeString(code) {
+  switch (code) {
+    case 0: return "Clear sky";
+    case 1: return "Mainly clear";
+    case 2: return "Mainly partly cloudy";
+    case 3: return "Mainly overcast";
+    case 45: return "Fog";
+    case 48: return "Depositing rime fog";
+    case 51: return "Drizzle: Light";
+    case 53: return "Drizzle: moderate";
+    case 51: return "Drizzle: dense intensity";
+    case 56: return "Freezing Drizzle: Light";
+    case 57: return "Freezing Drizzle: dense intensity";
+    case 61: return "Rain: Slight";
+    case 63: return "Rain: moderate";
+    case 65: return "Rain: heavy intensity";
+    case 66: return "Freezing Rain: Light";
+    case 67: return "Freezing Rain: heavy intensity";
+    case 71: return "Snow fall: Slight";
+    case 73: return "Snow fall: moderate";
+    case 75: return "Snow fall: heavy intensity";
+    case 77: return "Snow grains";
+    case 80: return "Rain showers: Slight";
+    case 81: return "Rain showers: moderate";
+    case 82: return "Rain showers: violent";
+    case 85: return "Snow showers slight";
+    case 86: return "Snow heavy";
+    case 95: return "Thunderstorm: Slight or moderate";
+    case 96: return "Thunderstorm with slight hail";
+    case 99: return "Thunderstorm with heavy hail";
+    default: return "Wrong code";
+  }
+}
+
+/**
+ * 
+ * @param {*} code 
+ * @returns 
+ */
+function getICONFromWeatherCode(code) {
+  switch (code) {
+    case 0: return iconPathClearSky;
+    case 1: return iconPathMainly;
+    case 2: return iconPathMainly;
+    case 3: return iconPathMainly;
+    case 45: return iconPathFog;
+    case 48: return iconPathFog;
+    case 51: return iconPathDrizzle;
+    case 53: return iconPathDrizzle;
+    case 55: return iconPathDrizzle;
+    case 56: return iconPathFreezingDrizzle;
+    case 57: return iconPathFreezingDrizzle;
+    case 61: return iconPathRain;
+    case 63: return iconPathRain;
+    case 65: return iconPathRain;
+    case 66: return iconPathFreezingRain;
+    case 67: return iconPathFreezingRain;
+    case 71: return iconPathSnow;
+    case 73: return iconPathSnow;
+    case 75: return iconPathSnow;
+    case 77: return iconPathSnow;
+    case 80: return iconPathRainShowers;
+    case 81: return iconPathRainShowers;
+    case 82: return iconPathRainShowers;
+    case 85: return iconPathSnowHeavy;
+    case 86: return iconPathSnowHeavy;
+    case 95: return iconPathThunderstorm;
+    case 96: return iconPathThunderstormHail;
+    case 99: return iconPathThunderstormHail;
+    default: return "Wrong code";
+  }
+}
+
+
 function addHTMLelements(objectIP, objectWeather) {
   const { country, region, city } = objectIP;
   const { current_weather } = objectWeather;
-  const { temperature, windspeed, winddirection, time, weathercode } =
-    current_weather;
-  divElement.style.backgroundImage = `url(${getBGImageFromTime(time)})`;
-  divElement.style.backgroundSize = "100%"
-  const imgElement = document.createElement("img");
-  imgElement.src = getICONFromWeatherCode(weathercode);
-  imgElement.className = "img-style";
+  const { temperature, windspeed, winddirection, time, weathercode } = current_weather;
+
+  const imgToWeatherElement = document.createElement("img");
+  imgToWeatherElement.src = getICONFromWeatherCode(weathercode);
+  imgToWeatherElement.className = "imgToWeatherElement-style";
+
   const countryElement = document.createElement("p");
   countryElement.textContent = `${country}, ${city}`;
   countryElement.style.fontWeight = "bold";
   countryElement.style.marginBottom = 0;
+
   const iconTempElement = document.createElement("img");
-  iconTempElement.src = "./image/icon-temp.png";
-  iconTempElement.style.height = "20px";
+  iconTempElement.src = iconTemperatur;
+  iconTempElement.style.height = "25px";
+
   const iconWindsElement = document.createElement("img");
-  iconWindsElement.src = "./image/winds.png";
+  iconWindsElement.src = iconWind;
   iconWindsElement.style.height = "20px";
   iconWindsElement.style.marginLeft = "3px";
+
   const iconWindDirectionElement = document.createElement("img");
-  iconWindDirectionElement.src = "./image/down.png";
+  iconWindDirectionElement.src = iconArrow;
   iconWindDirectionElement.style.transform = `rotate(${winddirection}deg)`;
   iconWindDirectionElement.style.height = "20px";
-  iconWindDirectionElement.style.marginLeft = "3px";
+  iconWindDirectionElement.style.margin = "3px";
+
   const regionElement = document.createElement("p");
   regionElement.textContent = region;
   regionElement.style.fontSize = "20px";
@@ -53,29 +177,46 @@ function addHTMLelements(objectIP, objectWeather) {
 
   const tempElement = document.createElement("span");
   tempElement.textContent = `${temperature}°C`;
+  tempElement.style.display = "flex";
+  tempElement.style.justifyContent = "center";
+  tempElement.style.alignItems = "center";
   tempElement.append(iconTempElement);
-  const windsElement = document.createElement("span");
-  windsElement.textContent = `${windspeed} km/h`;
-  windsElement.append(iconWindsElement);
-  windsElement.style.margin = "5px";
-  const winDirectionElement = document.createElement("span");
-  winDirectionElement.textContent = getImageWindDirection(winddirection);
-  winDirectionElement.append(iconWindDirectionElement);
-  winDirectionElement.style.margin = "5px";
+
+  const windElement = document.createElement("span");
+  windElement.textContent = `${windspeed} km/h`;
+  windElement.style.margin = "5px";
+  windElement.style.display = "flex";
+  windElement.style.justifyContent = "center";
+  windElement.style.alignItems = "center";
+  windElement.append(iconWindsElement);
+
+  const windDirectionElement = document.createElement("span");
+  windDirectionElement.textContent = getImageWindDirection(winddirection);
+  windDirectionElement.style.margin = "5px";
+  windDirectionElement.style.whiteSpace = "nowrap";
+  windDirectionElement.style.display = "flex";
+  windDirectionElement.style.justifyContent = "center";
+  windDirectionElement.style.alignItems = "center";
+  windDirectionElement.append(iconWindDirectionElement);
 
   const infoWindElement = document.createElement("p");
-  infoWindElement.append(windsElement, winDirectionElement);
   infoWindElement.style.fontSize = "20px";
   infoWindElement.style.margin = "5px";
-  infoWindElement.style.borderTop = "1px solid";
+  infoWindElement.style.borderTop = "1px solid black";
   infoWindElement.style.padding = "3px";
-  const codeElement = document.createElement("p");
-  codeElement.textContent = getWeatherCodeString(weathercode);
-  codeElement.style.fontSize = "15px";
-  codeElement.style.margin = "3px";
+  infoWindElement.style.display = "flex";
+  infoWindElement.style.justifyContent = "center";
+  infoWindElement.style.alignItems = "center";
+  infoWindElement.append(windElement, windDirectionElement);
+
+  const valueToCodeWeatherElement = document.createElement("p");
+  valueToCodeWeatherElement.textContent = getWeatherCodeString(weathercode);
+  valueToCodeWeatherElement.style.fontSize = "15px";
+  valueToCodeWeatherElement.style.margin = "3px";
 
   const leftColumn = document.createElement("td");
-  leftColumn.append(imgElement, codeElement);
+  leftColumn.append(imgToWeatherElement, valueToCodeWeatherElement);
+
   const rightColumn = document.createElement("td");
   rightColumn.style.maxWidth = "350px";
   rightColumn.append(
@@ -84,151 +225,15 @@ function addHTMLelements(objectIP, objectWeather) {
     tempElement,
     infoWindElement
   );
+
   const rowElement = document.createElement("tr");
   rowElement.append(leftColumn, rightColumn);
+
   const tableElement = document.createElement("table");
   tableElement.className = "table-style";
   tableElement.append(rowElement);
+
+  divElement.style.backgroundImage = `url(${getBGImageFromTime(time)})`;
+  divElement.style.backgroundSize = "100%";
   divElement.append(tableElement);
 }
-
-function getWeatherCodeString(code) {
-  switch (code) {
-    case 0:
-      return "Clear sky";
-    case 1:
-      return "Mainly clear";
-    case 2:
-      return "Mainly partly cloudy";
-    case 3:
-      return "Mainly overcast";
-    case 45:
-      return "Fog";
-    case 48:
-      return "Depositing rime fog";
-    case 51:
-      return "Drizzle: Light";
-    case 53:
-      return "Drizzle: moderate";
-    case 51:
-      return "Drizzle: dense intensity";
-    case 56:
-      return "Freezing Drizzle: Light";
-    case 57:
-      return "Freezing Drizzle: dense intensity";
-    case 61:
-      return "Rain: Slight";
-    case 63:
-      return "Rain: moderate";
-    case 65:
-      return "Rain: heavy intensity";
-    case 66:
-      return "Freezing Rain: Light";
-    case 67:
-      return "Freezing Rain: heavy intensity";
-    case 71:
-      return "Snow fall: Slight";
-    case 73:
-      return "Snow fall: moderate";
-    case 75:
-      return "Snow fall: heavy intensity";
-    case 77:
-      return "Snow grains";
-    case 80:
-      return "Rain showers: Slight";
-    case 81:
-      return "Rain showers: moderate";
-    case 82:
-      return "Rain showers: violent";
-    case 85:
-      return "Snow showers slight";
-    case 86:
-      return "Snow heavy";
-    case 95:
-      return "Thunderstorm: Slight or moderate";
-    case 96:
-      return "Thunderstorm with slight";
-    case 99:
-      return "Thunderstorm with heavy hail";
-    default:
-      return "Wrong code";
-  }
-}
-
-function getICONFromWeatherCode(code) {
-  switch (code) {
-    case 0:
-      return "./image/codeWeather/113.png";
-    case 1:
-      return "./image/codeWeather/116.png";
-    case 2:
-      return "./image/codeWeather/116.png";
-    case 3:
-      return "./image/codeWeather/116.png";
-    case 45:
-      return "./image/codeWeather/143.png";
-    case 48:
-      return "./image/codeWeather/143.png";
-    case 51:
-      return "./image/codeWeather/185.png";
-    case 53:
-      return "./image/codeWeather/185.png";
-    case 55:
-      return "./image/codeWeather/185.png";
-    case 56:
-      return "./image/codeWeather/182.png";
-    case 57:
-      return "./image/codeWeather/182.png";
-    case 61:
-      return "./image/codeWeather/308.png";
-    case 63:
-      return "./image/codeWeather/308.png";
-    case 65:
-      return "./image/codeWeather/308.png";
-    case 66:
-      return "./image/codeWeather/230.png";
-    case 67:
-      return "./image/codeWeather/230.png";
-    case 71:
-      return "./image/codeWeather/338.png";
-    case 73:
-      return "./image/codeWeather/338.png";
-    case 75:
-      return "./image/codeWeather/338.png";
-    case 77:
-      return "./image/codeWeather/338.png";
-    case 80:
-      return "./image/codeWeather/359.png";
-    case 81:
-      return "./image/codeWeather/359.png";
-    case 82:
-      return "./image/codeWeather/359.png";
-    case 85:
-      return "./image/codeWeather/371.png";
-    case 86:
-      return "./image/codeWeather/371.png";
-    case 95:
-      return "./image/codeWeather/386.png";
-    case 96:
-      return "./image/codeWeather/392.png";
-    case 99:
-      return "./image/codeWeather/392.png";
-    default:
-      return "Wrong code";
-  }
-}
-
-async function getIPandWeather() {
-  const res1 = await fetch("https://get.geojs.io/v1/ip/geo.json");
-  const objectIP = await res1.json();
-  const { longitude, latitude } = objectIP;
-
-  const res2 = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
-  );
-  const objectWeather = await res2.json();
-
-  addHTMLelements(objectIP, objectWeather);
-}
-
-getIPandWeather();
